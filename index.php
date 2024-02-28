6fefd507fda30c6d7ee1cfb57662e0a8fa2308d3
Tien Nguyen <tiennguyen3354@gmail.com>
2024-02-27 21:18:20 -0500

    updated the index page and add tmp to gitignore and update classes for composer update

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 // this will be the controller page .
 //this project will be working with fat free .
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once ('vendor/autoload.php');
+
+//initialize the fat free
+$f3 = Base::instance(); // this is a static method
+
+//initialize the controller class
+$controller = new Controller($f3);
+
+//routing to home /
+$f3->route('GET /', function () {
+    $GLOBALS['controller']->home();
+});
+
+//routing to hours form
+$f3->route('GET|POST /hours', function () {
+    $GLOBALS['controller']->hours();
+});
+$f3->run();
